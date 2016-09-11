1ce197eb23d17a9c2c7a1b8fa95d8ab224b8385f
Christian Scheb <info@stylemotion.de>
2016-09-11 19:49:20 +0200

    Update test

--- Tests/SchebTwoFactorBundleTest.php
+++ Tests/SchebTwoFactorBundleTest.php
@@ -15,9 +15,24 @@ class SchebTwoFactorBundleTest extends TestCase
 
         //Expect compiler pass to be added
         $containerBuilder
-            ->expects($this->once())
+            ->expects($this->exactly(2))
             ->method('addCompilerPass')
-            ->with($this->isInstanceOf('Scheb\TwoFactorBundle\DependencyInjection\Compiler\ProviderCompilerPass'));
+            ->with($this->logicalOr(
+                $this->isInstanceOf('Scheb\TwoFactorBundle\DependencyInjection\Compiler\FirewallCompilerPass'),
+                $this->isInstanceOf('Scheb\TwoFactorBundle\DependencyInjection\Compiler\ProviderCompilerPass')
+            ));
+
+        //Expect register authentication provider factory
+        $securityExtension = $this->createMock('Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension');
+        $containerBuilder
+            ->expects($this->once())
+            ->method('getExtension')
+            ->with('security')
+            ->willReturn($securityExtension);
+        $securityExtension
+            ->expects($this->once())
+            ->method('addSecurityListenerFactory')
+            ->with($this->isInstanceOf('\Scheb\TwoFactorBundle\DependencyInjection\Factory\Security\TwoFactorFactory'));
 
         $bundle = new SchebTwoFactorBundle();
         $bundle->build($containerBuilder);
